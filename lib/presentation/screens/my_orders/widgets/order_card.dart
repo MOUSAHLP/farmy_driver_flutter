8798c55c3ba8005d95b26b58b7d7f2da602f9996
layan eias <layaneias@gmail.com>
2024-02-21 11:57:52 +0300

    fix merge

--- lib/presentation/screens/my_orders/widgets/order_card.dart
+++ lib/presentation/screens/my_orders/widgets/order_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pharmy_driver/core/app_router/app_router.dart';
